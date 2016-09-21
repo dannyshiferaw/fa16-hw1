a1b5e9aae7a7f67e45ebb353a21a4614b6b2c6b6
dannyshiferaw <daniel.shiferaw@berkeley.edu>
2016-09-21 11:11:58 -0700

    HW 1

--- foobar.rb
+++ foobar.rb
@@ -1,9 +1,16 @@
 class Foobar
-
-  def self.baz(a)
+    
+    def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
-  end
+    total = 0
+    a.uniq
+    a.map! {|x| x.to_i + 2}
+    a.keep_if { |x| x % 2 == 0 and x <= 10}
+    a.each { |x| total += x}
+    return total
+    
+end
 end
 
 
